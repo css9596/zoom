5d75626997a6d67b6181b00ab238062fde9ccbd5
Choi Sung Sik <css9596@naver.com>
2021-12-27 20:12:02 +0900

    Zoom Clone Coding Start!

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+
+const handleListen = () => console.log('Listening on http://localhost:3000');
+app.listen(3000);
