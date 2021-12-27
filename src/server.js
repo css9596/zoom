633460d3b4bf4d7457ebf01fdb4d656764780db0
Choi Sung Sik <css9596@naver.com>
2021-12-28 02:17:11 +0900

    Zoom Clone Coding Start!

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,31 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req,res) => res.redirect("/"));
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);  // http Server
+
+const sockets = [];
+
+const wss = new WebSocket.Server({server}); //WebSocket Server
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "noname";
+    console.log("Connected to Browsers");
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+            default:
+        }
+    });
+    socket.on("close", () => console.log("Disconnected from the Browser"));
+});
+
+server.listen(3000, handleListen);
